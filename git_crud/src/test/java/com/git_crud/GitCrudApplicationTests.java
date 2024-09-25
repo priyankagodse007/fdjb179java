585a0f68b611498a774dc66c47c8388af1ef5320
priyankagodse007 <priyankagodse007@gmail.com>
2024-09-25 16:38:36 +0530

    by priyanka

---
+++ git_crud/src/test/java/com/git_crud/GitCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
